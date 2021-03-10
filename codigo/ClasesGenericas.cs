ec6aeeea73c8b96b06c3a4a611eb0a7405e6c42c
Pablo Bruno <hola@pablobrunodw.com>
2021-03-10 12:10:48 -0300

    Bug de la Id del Lote

--- codigo/ClasesGenericas.cs
+++ codigo/ClasesGenericas.cs
@@ -707,7 +707,7 @@ namespace DKdll.codigo
             if (pObjLote != null)
             {
                 resultado = new cLote();
-                resultado.ID = pObjLote.Id;
+                resultado.ID = pObjLote.ID;
                 resultado.NombreProducto = pObjLote.NombreProducto;
                 resultado.NumeroLote = pObjLote.NumeroLote;
                 resultado.FechaVencimiento = pObjLote.FechaVencimiento;
